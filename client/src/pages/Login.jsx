6318063daaa941c5d664791c0ed1c65ab8244bb3
George Cheng <sparklyspuds@gmail.com>
2022-05-21 20:08:35 +0800

    Register Page added

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,67 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("https://i.imgur.com/OI44MiS.png") center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+  padding: 20px;
+  width: 40%;
+  background-color: white;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+`;
+
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0 0;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+`;
+
+const Link =styled.a``
+
+const Login = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder="username" />
+          <Input placeholder="password" />
+          <Button>LOGIN</Button>
+          <Link>Forget Password</Link>
+          <Link>Create A New Account</Link>
+        </Form>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Login
