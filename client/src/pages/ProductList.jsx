7c4b19bd2b1e00635ab5e817761d9ef4733cef35
George Cheng <sparklyspuds@gmail.com>
2022-05-21 13:57:35 +0800

    ProductList added

--- client/src/pages/ProductList.jsx
+++ client/src/pages/ProductList.jsx
@@ -1,13 +1,80 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const Container=styled.div``
+import Announcement from "../components/Annoucement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Products from "../components/Products";
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+`;
+
+const FilterText = styled.span`
+  font-size: 20;
+  font-weight: 600;
+  margin-right: 20px;
+`;
+
+const Select = styled.select`
+padding: 10px;
+margin-right: 20px;`;
+
+const Option = styled.option``;
 
 const ProductList = () => {
   return (
     <Container>
-        
-    </Container>
-  )
-}
+      <Navbar />
+      <Announcement />
+      <Title>Dresses</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter Products:</FilterText>
+        </Filter>
 
-export default ProductList
+        {/* <Select name="color" onChange={handleFilters}>
+            <Option disabled>Color</Option>
+            <Option>white</Option>
+            <Option>black</Option>
+            <Option>red</Option>
+            <Option>blue</Option>
+            <Option>yellow</Option>
+            <Option>green</Option>
+        </Select> */}
+        {/* <Select name="size" onChange={handleFilters}>
+            <Option disabled>Size</Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select> */}
+
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+          <Select>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (desc)</Option>
+          </Select>
+        </Filter>
+
+      </FilterContainer>
+      <Products />
+      <Footer />
+    </Container>
+  );
+};
+
+export default ProductList;
