4cacf551cb4a74bffa029094259355094dcc5643
George Cheng <sparklyspuds@gmail.com>
2022-05-22 13:11:22 +0800

    Navbar responsive adjusted

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import styled from "styled-components";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import { Badge } from "@material-ui/core";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
   height: 60px;
+  ${mobile({ height: "50px" })}
 `;
 
 const Wrapper = styled.div`
@@ -12,6 +14,7 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  ${mobile({ padding: "10px 0px" })}
 `;
 
 const Left = styled.div`
@@ -22,6 +25,7 @@ const Left = styled.div`
 
 const Language = styled.span`
   font-size: 14px;
+  ${mobile({ display: "none" })}
 `;
 
 const SearchContainer = styled.div`
@@ -34,6 +38,7 @@ const SearchContainer = styled.div`
 
 const Input = styled.input`
   border: none;
+  ${mobile({ width: "50px" })}
 `;
 
 const Center = styled.div`
@@ -43,18 +48,24 @@ const Center = styled.div`
 
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({ fontSize: "16px" })}
+
 `;
 const Right = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  ${mobile({ flex:2,justifyContent: "Center" })}
+
 `;
 
 const Menuitem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
+  ${mobile({ fontSize: "12px" ,marginLeft:"10px"})}
+
 `;
 
 const Navbar = () => {
@@ -64,8 +75,8 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input />
-            <Search style={{color:"gray",fontSize:16}}/>
+            <Input placeholder="search"/>
+            <Search style={{ color: "gray", fontSize: 16 }} />
           </SearchContainer>
         </Left>
 
