595b309e519b2454334a716889a5e0481eea9694
George Cheng <sparklyspuds@gmail.com>
2022-05-26 20:30:09 +0800

    Merging

--- Project-III/server/models/index.js
+++
@@ -1,7 +0,0 @@
-const User = require("./User");
-const Product = require("./Product");
-const Cart = require("./Cart");
-const Order = require("./Order");
-const Category = require("./Category");
-
-module.exports = { User, Product, Cart, Order, Category };
